ae5d242d272af5062a3380b14e6c19d3f451b1df
Wojciech Larecki <wojtek.larecki@gmail.com>
2021-02-18 15:33:08 +0100

    init commit

---
+++ GitPractise_Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitPractiseDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
